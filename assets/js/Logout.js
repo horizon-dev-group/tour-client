3cec20bee40ab650e72fe7aaeea188ac48ecc454
solomonmulatu <Solomon Mulatu>
2020-04-15 17:11:49 +0300

    ui and logout updated

--- assets/js/Logout.js
+++ assets/js/Logout.js
@@ -3,7 +3,7 @@ var logoutLink = document.getElementById('logout');
 logoutLink.onclick = function performLogout() {
 
     localStorage.removeItem('token');
-    window.location.replace("https://tour-client.herokuapp.com");
+    window.location.replace("https://tour-client.herokuapp.com/login.html");
 
 
 };
